7ca8045dd37b1f500091cbf804f8abfc018c40a8
Matthew Horwood <matt@horwood.biz>
2023-11-12 14:44:04 +0000

    All new testing tools

--- testing/test.sh
+++ testing/test.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 
-. ~/etc/colours
-
 TOP="$(git rev-parse --show-toplevel)"
+. ${TOP}/testing/colours
 
-function help(){
+help() {
   echo "${green}This is the testing tool${NC}"
-  echo "$0 ${purple}[full/create/destroy/ssh]${NC}"
+  echo "$0 ${purple}[full/create/destroy/converge]${NC}"
   echo "${green}You can also SSH to a single host using $0 ssh${NC}"
   exit 1
 }
 
-function tasks(){
+tasks() {
   cowthink -p -W 60 "COMMENT [$1]"
   echo "${2}ok: [localhost]${NC}"
 }
 
-function create(){
+create() {
   tasks "Building instance ..." "${BLUE}"
-  ansible-playbook ${TOP}/testing/playbooks/create.yml &&
-  ansible-playbook -i ${TOP}/testing/inventory ${TOP}/testing/playbooks/prepare.yml ||
+  ansible-playbook -i ${TOP}/testing/inventory --extra-vars "top_dir=${TOP}" ${TOP}/testing/playbooks/create-pve.yml &&
+  ansible-playbook -i ${TOP}/testing/inventory --extra-vars "top_dir=${TOP}" ${TOP}/testing/playbooks/prepare.yml ||
   tasks "That errored" "${red}"
   tasks "Create and prep done, now running the test playbook" "${green}"
 }
 
-function destroy(){
+destroy() {
   tasks "just check for an instance and clean it up" "${BLUE}"
-  ansible-playbook ${TOP}/testing/playbooks/destroy.yml ||
+  ansible-playbook -i ${TOP}/testing/inventory --extra-vars "top_dir=${TOP}" ${TOP}/testing/playbooks/destroy-pve.yml ||
   tasks "That errored" "${red}"
   tasks "All clean" "${green}"
 }
+
 case "$1" in
   "full")
     destroy
@@ -38,9 +38,9 @@ case "$1" in
     ansible-lint . ||
     tasks "You need to fix that lint" "${red}"
     create
-    ansible-playbook -i ${TOP}/testing/inventory ./molecule/default/converge.yml ||
+    ansible-playbook -i ${TOP}/testing/ansible_inventory.yml ./molecule/default/converge.yml ||
     tasks "That errored" "${red}"
-    ansible-playbook -i ${TOP}/testing/inventory ./molecule/default/verify.yml ||
+    ansible-playbook -i ${TOP}/testing/ansible_inventory.yml ./molecule/default/verify.yml ||
     tasks "That errored" "${red}"
     destroy
     tasks "testing complete" "${green}"
@@ -54,17 +54,27 @@ case "$1" in
   "lint")
     tasks "Linting Yaml files..." "${BLUE}"
     ansible-lint . ||
-      tasks "You need to fix that lint" "${red}"
+    tasks "You need to fix that lint" "${red}"
     ;;
   "-h")
     help
     ;;
-  *)
+  converge)
     tasks "Start of testing" "${BLUE}"
-    ansible-playbook -i ${TOP}/testing/inventory ./molecule/default/converge.yml ||
+    ansible-playbook -i ${TOP}/testing/ansible_inventory.yml ./molecule/default/converge.yml ||
     tasks "That errored" "${red}"
-    ansible-playbook -i ${TOP}/testing/inventory ./molecule/default/verify.yml ||
+    ansible-playbook -i ${TOP}/testing/ansible_inventory.yml ./molecule/default/verify.yml ||
     tasks "That errored" "${red}"
     tasks "testing complete" "${green}"
     ;;
+  *)
+    create
+    ansible-playbook -i ${TOP}/testing/ansible_inventory.yml ./molecule/default/converge.yml ||
+    tasks "That errored" "${red}"
+    ansible-playbook -i ${TOP}/testing/ansible_inventory.yml ./molecule/default/verify.yml ||
+    tasks "That errored" "${red}"
+    destroy
+    tasks "testing complete" "${green}"
+    ;;
+
 esac
