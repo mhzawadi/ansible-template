56195b0ebfff040efb6fd98d94f56965a3e16f75
Matthew Horwood <matt@horwood.biz>
2024-01-06 16:05:08 +0000

    bug: fix environment setup

--- testing/lint.sh
+++ testing/lint.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+if [ ! -d .venv ]
+then
+  python3 -m venv .venv
+  source .venv/bin/activate
+  python3 -m pip install --upgrade pip
+  python3 -m pip install -r requirements.txt
+  ansible-galaxy install -r requirements.yml
+elif [ -z "$VIRTUAL_ENV" ]
+then
+  source .venv/bin/activate
+fi
+
 directory=`pwd`
 
 readonly CWD=$directory
